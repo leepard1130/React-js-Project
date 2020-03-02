90156650e0fa383202998f63e956603734f904e1
LAPTOP-7GO5N953\nowit <dn6605@hotmail.com>
2020-03-03 00:51:13 +1000

    component life cycle updated

--- Udemy-prac/prac-project/src/components/Persons/Persons.js
+++ Udemy-prac/prac-project/src/components/Persons/Persons.js
@@ -1,14 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Person from './Person/Person';
 
-const persons = (props) => props.persons.map((person, index) => {
-    return <Person
-        click={() => props.clicked(index)}
-        name={person.name}
-        age={person.age}
-        key={person.id}
-        changed={event => props.changed(event, person.id)}
-      />
-  })
+class Persons extends Component  {
+  // static getDerivedStateFromProps (props, state) {
+  //   return state;
+  // };
 
-export default persons
+  componentWillReceiveProps(props) {
+    console.log('Persons.js:componentWillReceiveProps',props);
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('Persons.js:shouldComponentUpdate');
+    return true;
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log('Persons.js:getSnapshotBeforeUpdate');
+    return null;
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log('Persons.js:componentDidUpdate',snapshot);
+  }
+
+  render() {
+    return (this.props.persons.map((person, index) => {
+      return <Person
+          click={() => this.props.clicked(index)}
+          name={person.name}
+          age={person.age}
+          key={person.id}
+          changed={event => this.props.changed(event, person.id)}
+        />
+    }));
+  }
+};
+
+export default Persons
