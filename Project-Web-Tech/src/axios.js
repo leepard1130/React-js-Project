ce3258e21691f087b6a7680d0dc181f8166385ab
LAPTOP-7GO5N953\nowit <dn6605@hotmail.com>
2020-05-24 02:08:01 +1000

    create and use axios instance in blog.js

---
+++ Project-Web-Tech/src/axios.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+const instance = axios.create({
+    baseURL: 'https://jsonplaceholder.typicode.com'
+});
+
+instance.defaults.headers.common['Authorizattion'] = 'AUTH TOKEN FROM';
+
+export default instance;
