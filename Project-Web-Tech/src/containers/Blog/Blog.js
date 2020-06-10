ae616afe3c9f96e236cfbf0a3c03a44634b5c1f2
LAPTOP-7GO5N953\nowit <dn6605@hotmail.com>
2020-06-10 19:27:43 +1000

    rendering components

--- Project-Web-Tech/src/containers/Blog/Blog.js
+++ Project-Web-Tech/src/containers/Blog/Blog.js
@@ -17,7 +17,8 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <Route path = "/" render = { ()=><h1>home</h1> } />
+                {/* <Route path = "/" render = { ()=><h1>home</h1> } /> */}
+                <Route path = "/" exact component = {Posts} />
             </div>
         );
     }
