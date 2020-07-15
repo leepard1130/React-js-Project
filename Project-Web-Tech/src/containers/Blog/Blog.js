bcf111b1150c788b6fc32d8259c8c4f4fb59b3d1
LAPTOP-7GO5N953\nowit <dn6605@hotmail.com>
2020-07-15 21:54:11 +1000

    pass route parameters and get the id of each post

--- Project-Web-Tech/src/containers/Blog/Blog.js
+++ Project-Web-Tech/src/containers/Blog/Blog.js
@@ -3,6 +3,7 @@ import { Route, NavLink } from 'react-router-dom';
 import './Blog.css';
 import Posts from '../Blog/Posts/Posts';
 import NewPost from '../Blog/NewPost/NewPost';
+import FullPost from './FullPost/FullPost';
 
 class Blog extends Component {
 
@@ -33,6 +34,7 @@ class Blog extends Component {
                 {/* <Route path = "/" render = { ()=><h1>home</h1> } /> */}
                 <Route path = "/" exact component = {Posts} />
                 <Route path = "/new-post" exact component = {NewPost} />
+                <Route path = "/:id" exact component = {FullPost} />
             </div>
         );
     }
