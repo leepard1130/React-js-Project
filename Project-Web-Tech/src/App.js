b1d8f7c0b289558394d4cf0802760f6fd8a425e4
LAPTOP-7GO5N953\nowit <dn6605@hotmail.com>
2020-07-24 19:04:51 +1000

    using asyncComponet to load components asynchronously

--- Project-Web-Tech/src/App.js
+++ Project-Web-Tech/src/App.js
@@ -5,6 +5,8 @@ import Blog from './containers/Blog/Blog';
 class App extends Component {
   render() {
     return (
+      //basename 可以為所有位置添加一個基準的url 例如這邊就是localhost: /my-app
+      // <BrowserRouter basename = "/my-app" />
       <BrowserRouter>
         <div className="App">
           <Blog />
--- Project-Web-Tech/src/containers/Blog/Blog.js
+++ Project-Web-Tech/src/containers/Blog/Blog.js
@@ -2,12 +2,18 @@ import React, { Component } from 'react';
 import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 import './Blog.css';
 import Posts from '../Blog/Posts/Posts';
-import NewPost from '../Blog/NewPost/NewPost';
+//import NewPost from '../Blog/NewPost/NewPost';
+import asyncComponent from '../../hoc/asyncComponent';
+//使用AsyncComponent可以選擇性load某個component不用一次loading
+//可以到network的performance檢查是否成功
+const AsyncNewPost = asyncComponent(() => {
+    return import('./NewPost/NewPost');
+});
 
 class Blog extends Component {
 
     state = {
-        auth: false
+        auth: true
     }
 
     render () {
@@ -40,7 +46,7 @@ class Blog extends Component {
                 {/* <Route path = "/" render = { ()=><h1>home</h1> } /> */}
                 {/* // 使用SWITCH可以只LOADING其中一個但是在這個案例中new-post跟id的順序不能對調 不然會沒辦法loadingnew-post */}
                 <Switch>
-                    {this.state.auth? <Route path = "/new-post" component = {NewPost} /> : null }    
+                    {this.state.auth? <Route path = "/new-post" component = {AsyncNewPost} /> : null }    
                     <Route path = "/posts" component = {Posts} />
                     <Route render = { () => <h1>Not Found!</h1> } />
                     {/* <Redirect from = "/" to = "/posts" /> */}
