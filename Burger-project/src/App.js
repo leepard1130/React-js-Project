d1abb530b965fb42fce23a32c3873c51302393e1
LAPTOP-7GO5N953\nowit <dn6605@hotmail.com>
2020-07-26 08:11:39 +1000

    adding route to the projet

--- Burger-project/src/App.js
+++ Burger-project/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 
+import { Route, Switch } from 'react-router-dom';
 import Layout from './hoc/layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
@@ -9,8 +10,10 @@ class App extends Component {
     return (
       <div>
         <Layout>
-          <BurgerBuilder />
-          <Checkout />
+          <Switch>
+            <Route path = "/" exacct component = { BurgerBuilder } />
+            <Route path = "/checkout" component = { Checkout } />
+          </Switch>
         </Layout>
       </div>
     );
