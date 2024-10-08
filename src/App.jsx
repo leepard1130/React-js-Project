177e0dfed4c009e46791d465dab483d79ce18ad5
Eric Lee <ericlee@192-168-1-104.tpgi.com.au>
2024-10-08 22:40:59 +1000

    added NewProject & Input component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import NewProject from "./components/NewProject.jsx";
 import ProjectsSideBar from "./components/ProjectsSideBar.jsx";
+
 function App() {
 
   return (
-    <main>
+    <main className="h-screen my-8 flex gap-8">
       <ProjectsSideBar />
+      <NewProject />
     </main>
   );
 }
