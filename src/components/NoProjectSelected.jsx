0563f32c7f736b0adceecc6efc12836deede5649
Eric Lee <ericlee@192-168-1-104.tpgi.com.au>
2024-10-12 23:33:02 +1000

    Collecting user input with refs and forwarded refs

--- src/components/NoProjectSelected.jsx
+++ src/components/NoProjectSelected.jsx
@@ -3,7 +3,7 @@ import Button from './Button.jsx';
 
 export default function NoProjectImage({onStartAddProject}) {
     return(
-        <duv className="mt-24 text-center w-2/3">
+        <div className="mt-24 text-center w-2/3">
             <img src={noProjectImage} 
                 alt="An empty task list"
                 className='w-16 h-16 object-contain mx-auto'
@@ -13,6 +13,6 @@ export default function NoProjectImage({onStartAddProject}) {
             <p className='mt-8'>
                 <Button onClick={onStartAddProject}>Create a new project</Button>
             </p>
-        </duv>
+        </div>
     );
 }
