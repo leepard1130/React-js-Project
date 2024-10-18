61f25bf19fd3c9cdc6d12f4d3a2b69576fd976e4
Eric Lee <ericlee@192-168-1-104.tpgi.com.au>
2024-10-18 23:42:39 +1000

    Styled Modal and Activated Cancel button

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,15 @@ function App() {
     });
   }
 
+  function handleCancelAddProject() {
+    setProjectState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+      }
+    });
+  }
+
   function handleAddProject(projectData) {
     setProjectState(prevState => {
       const newProject = {
@@ -33,11 +42,11 @@ function App() {
       };
     });
   }
-  console.log(projectState);
+  // console.log(projectState);
   let content;
   
   if(projectState.selectedProjectId === null) {
-    content = <NewProject onAdd={handleAddProject}/>
+    content = <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject}/>
   } else if (projectState.selectedProjectId === undefined) {
     content = <NoProjectSelected onStartAddProject={handleStartAddProject}/>
   }
--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -2,7 +2,7 @@ import { useRef } from 'react';
 import Input from './Input.jsx';
 import Modal from './Modal.jsx';
 
-export default function NewProject({onAdd}) {
+export default function NewProject({onAdd, onCancel}) {
     const modal = useRef();
 
     const title = useRef();
@@ -33,14 +33,21 @@ export default function NewProject({onAdd}) {
     return (
         <>
             <Modal ref={modal} buttonCaption='Okay'>
-                <h2>Invald Input</h2>
-                <p>Looks like you forgot to enter a value</p>
-                <p>Please make sure you provide a valid value for every input field</p>
+                <h2 className='text-xl font-bold text-stone-700 my-4'>Invald Input</h2>
+                <p className='text-stone-600 mb-4'>Looks like you forgot to enter a value</p>
+                <p className='text-stone-600 mb-4'>Please make sure you provide a valid value for every input field</p>
 
             </Modal>
             <div className='w-[35rem] mt-16'>
                 <menu className='flex items-center justify-end gap-4 my-4'>
-                    <li><button className='text-stone-800 hover-stone-950'>Cancel</button></li>
+                    <li>
+                        <button 
+                            onClick={onCancel}
+                            className='text-stone-800 hover-stone-950'
+                        >
+                            Cancel
+                        </button>
+                    </li>
                     <li>
                         <button 
                             className='px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950'
